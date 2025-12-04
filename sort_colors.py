78f3f93fc1181bf27328180f41f8aa8683ca7f96
Abrav74 <abrav74@gmail.com>
2025-12-03 19:18:35 -0800

    sortColors comp-leted

---
+++ sort_colors.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def sortColors(self, nums: List[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+
+    l = 0
+    r = len(nums) - 1
+    i = 0
+
+    while i <= r:
+        if nums[i] == 1:
+            i += 1
+        elif nums[i] == 0:
+            nums[i], nums[l] = nums[l], nums[i]
+            i += 1
+            l += 1
+        else:
+            nums[i], nums[r] = nums[r], nums[i]
+            r -= 1
+    return nums
