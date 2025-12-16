70b7cf7cd82eb011a498d4f79c9c143f7cc7e16c
Abrav74 <abrav74@gmail.com>
2025-12-15 18:14:28 -0800

    started longest consecutive

--- valid_sudoku_optimized.py
+++ valid_sudoku_optimized.py
@@ -16,16 +16,17 @@ def isValidSudoku(self, board: List[List[str]]) -> bool:
                 return False
             if board[row - 1][col - 1] in columns[col]:
                 return False
+            
             rows[row].append(board[row - 1][col - 1])
             columns[col].append(board[row - 1][col - 1])
 
             box_row = (row - 1) // 3
             box_col = (col - 1) // 3
-
             square = str(box_row) + str(box_col)
 
             if board[row - 1][col - 1] in squares[square]:
                 return False
+            
             squares[square].append(board[row - 1][col - 1])
 
     return True
